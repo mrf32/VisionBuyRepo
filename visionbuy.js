b9380c4fe3511021705f0b0ea4817cd5e44e7378
mrf32 <mrf32@njit.edu>
2026-02-27 15:36:31 +0000

    milestone 1

---
+++ visionbuy.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const fs = require('fs')
+
+const server = http.createServer(function(req, res) {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	try{
+		const html = fs.readFileSync('index.html');
+		res.write(html);
+		res.end();
+	} catch(e) {
+		res.statusCode = 404;
+		res.write("Bad request");
+		console.log(e);
+		res.end();
+	}
+})
+
+const port = 3001;
+
+server.listen(port, function() {
+	console.log("server is running on" + port);
+})
